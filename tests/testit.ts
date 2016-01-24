9eef268d241f6b7ecdcd5747e38bfe6273affb68
Ellery Newcomer <ellery-newcomer@utulsa.edu>
2016-01-24 13:16:44 -0800

    make failing test fail
    but for the right reason

--- tests/testit.ts
+++ tests/testit.ts
@@ -1,6 +1,6 @@
 /// <reference path="./jasmine/jasmine.d.ts" />
 describe("The Test", () => {
     it("should fail", () => {
-        expect("success").to("fail");
+        expect("failure").toBe("success");
     });
 });
