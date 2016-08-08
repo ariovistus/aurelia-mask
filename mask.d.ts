457591df6189c2b3030132242de821f668831e3d
ariovistus <ellery-newcomer@utulsa.edu>
2016-08-08 16:14:20 -0700

    sigh

---
+++ mask.d.ts
@@ -0,0 +1,4 @@
+export declare class MaskValueConverter {
+    toView(value: string, format: string, bindMasking: boolean, placeholder: string): string;
+    fromView(value: string, format: string, bindMasking: boolean, placeholder: string): string;
+}
---
+++ masker.d.ts
@@ -0,0 +1,34 @@
+export declare function getMasker(options: any): Masker;
+export declare class MaskOptions {
+    maskFormat: string;
+    bindMasking: boolean;
+    placeholder: string;
+    aspnetMasking: boolean;
+}
+export declare class Masker {
+    maskFormat: string;
+    maskCaretMap: Array<number>;
+    maskPatterns: Array<RegExp>;
+    maskPlaceholder: string;
+    minRequiredLength: number;
+    maskComponents: any;
+    maskProcessed: boolean;
+    bindMasking: boolean;
+    aspnetMasking: boolean;
+    placeholder: string;
+    constructor(options: MaskOptions);
+    unmaskValue(value: any): string;
+    maskValue(unmaskedValue: any): string;
+    maxCaretPos(value: any): number;
+    minCaretPos(): number;
+    _unmaskValue(value: any): string;
+    _maskValue(unmaskedValue: string, keepMasking: boolean): string;
+    _maskValue2(unmaskedValue: string): string;
+    stripPlaceholders(masked: any): string;
+    getNextCaretPos(caretPos: number): number;
+    getPreviousCaretPos(caretPos: number): number;
+    processRawMask(): void;
+    getMaskComponents(): void;
+    getPlaceholderChar(i: any): string;
+    isValidAt(chr: string, caretPos: number): boolean;
+}
