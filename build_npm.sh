e02188f3fc2fe8dd1fd23184889fd56090ff9a22
Ellery Newcomer <ellery-newcomer@utulsa.edu>
2017-12-08 12:21:54 -0800

    troubleshoot build

--- build_npm.sh
+++ build_npm.sh
@@ -1,7 +1,11 @@
 rm -rf dist/*
+ls dist
 docker run -v $(pwd):/app --entrypoint yarn sandrokeil/typescript install
 docker run -v $(pwd):/app --entrypoint python sandrokeil/typescript versionchecker.py
+echo "begin compile"
 docker run -v $(pwd):/app --entrypoint tsc sandrokeil/typescript -d -p . --outDir dist
+echo "end compile $?"
 docker run -v $(pwd):/app --entrypoint sh sandrokeil/typescript build_tsdecls_only.sh
 docker run -v $(pwd):/app --entrypoint npm sandrokeil/typescript pack
-ls
+ls dist
+cat dist/masked-input.js
